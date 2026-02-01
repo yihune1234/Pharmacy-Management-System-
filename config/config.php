2a1a39fd423af5afff70db8701b8ff149b572425
Yihune <yihunebelay859@gmail.com>
2026-02-01 21:37:26 +0300

    updated

--- config/config.php
+++ config/config.php
@@ -1,13 +1,11 @@
 <?php
 $host = "mysql-25712907-yihunebelay859-f8f5.h.aivencloud.com";
 $user = "avnadmin";
-$pass = getenv('DB_PASS');   // good practice 👍
-$db   = "defaultdb";         // must connect to existing DB first
-$port = 12671;               // correct port from Aiven
+$pass = getenv('DB_PASS');
+$db   = "pharmacy_db";
+$port = 25713;
 
 $conn = mysqli_init();
-
-/* Required SSL for Aiven */
 mysqli_ssl_set($conn, NULL, NULL, __DIR__ . "/ca.pem", NULL, NULL);
 
 if (!mysqli_real_connect($conn, $host, $user, $pass, $db, $port, NULL, MYSQLI_CLIENT_SSL)) {
